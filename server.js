56b34d16ebf46ed42d15e0be4e2ff4b7cf81efd2
Fiore20023 <fiorellasgagliardi@gmail.com>
2024-06-10 15:01:48 -0300

    prueba para reprar error de render

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
+const dotenv = require ('dotenv');
+dotenv.config()
 const cors = require('cors');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
+const password = process.env.PASSWORD;
 
 // Conectar a MongoDB
-mongoose.connect(process.env.MONGODB_URI, {"mongodb+srv://Fiorella:Prueba2024@cluster0.hdrctxp.mongodb.net/proyectoChayanne?retryWrites=true&w=majority&appName=Cluster0"
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Conectado a MongoDB'))
-  .catch(err => console.error('Error al conectar a MongoDB:', err));
+mongoose.connect("mongodb+srv://Fiorella:Prueba2024@cluster0.hdrctxp.mongodb.net/proyectoChayanne?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => console.log('Conectado a MongoDB'))
+    .catch(err => console.error('Error al conectar a MongoDB:', err));
+
+    console.log(process.env)
 
 // Middleware
 app.use(cors());
@@ -24,16 +24,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Importar y usar enrutadores
+app.use("/health", (req, res) => res.sendStatus(200));
 const albumRouter = require('./routers/albums');
 const userRouter = require('./routers/users'); // Agregada ruta usuario
 
 app.use('/albums', albumRouter);
 app.use('/users', userRouter); // Añadir rutas de usuarios
 
-// Ruta de salud
-app.use("/health", (req, res) => res.sendStatus(200));
-
 // Iniciar el servidor
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
+
